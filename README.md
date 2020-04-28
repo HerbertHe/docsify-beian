70246a7ba5be46b427cc536fb3b60b1820135510
HerbertHe <Herbert.He0229@gmail.com>
2020-04-28 11:10:53 +0800

    add: beian.js

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # docsify-beian
-docsify之备案插件
+
+<p align="center">
+  <img src="https://docsify.js.org/_media/icon.svg" />
+  <br />
+  <code>docsify-beian</code>
+</p>
+
+[![jsdelivr](https://data.jsdelivr.com/v1/package/npm/docsify-beian/badge)](https://www.jsdelivr.com/package/npm/docsify-beian)
+
+docsify之备案插件 docsify plugin for Chinese Beian
+
+## Usage
+
+添加依赖 Add script
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/docsify-beian@latest/beian.js"></script>
+```
+
+添加配置 Add settings
+
+```js
+window.$docsify = {
+  beian: {
+        ICP: "",
+        NISMSP: {
+            number: "",
+            url: "",
+        },
+    },
+}
+```
+
+| 属性名称 | 属性解释 | 默认值 |
+| --- | --- | --- |
+| ICP | 工信部ICP备案号 | "" |
+| NISMSP | 全国互联网安全管理服务平台备案（公安部备案） | {} |
+| number | 公安部备案备案号（没有留空）| "" |
+| url | 公安部备案链接（没有留空）| "" |
---
+++ dist/beian.js
@@ -0,0 +1,45 @@
+// 默认值
+var defaultOptions = {
+    ICP: "",
+    NISMSP: {
+        number: "",
+        url: "",
+    },
+}
+
+// Docsify plugin functions
+function plugin(hook, vm) {
+    hook.afterEach(function (html, next) {
+        //add html string
+        next(
+            html +
+                `
+            <hr>
+            <footer class="beian" style="text-align: center;">
+                <a style="text-decoration: none; color: #34495e; font-size: 15px; font-weight: 400;" href="http://www.beian.miit.gov.cn/" target="_blank">${
+                    defaultOptions.ICP
+                }</a>
+                ${
+                    defaultOptions.NISMSP.number.length !== 0 &&
+                    defaultOptions.NISMSP.url.length !== 0
+                        ? `
+                        &nbsp;&nbsp;
+                    <a style="text-decoration: none; color: #34495e; font-size: 15px; font-weight: 400;" href="${defaultOptions.NISMSP.url}" target="_blank">
+                        <img style="width: 15px; height: 15px;" src="http://www.beian.gov.cn/img/new/gongan.png" alt="全国互联网安全" />${defaultOptions.NISMSP.number}
+                    </a>
+                        `
+                        : ""
+                }
+
+            </footer>
+            `
+        )
+    })
+}
+
+// Docsify plugin options
+window.$docsify["beian"] = Object.assign(
+    defaultOptions,
+    window.$docsify["beian"]
+)
+window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins)
